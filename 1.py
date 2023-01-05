8a1bc5862073ec669b35f81b658ee437ca87207d
AnshFYIT <113037072+AnshFYIT@users.noreply.github.com>
2023-01-05 09:43:59 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,5 @@
+import tkinter as tk
+win=tk.Tk()
+win.minsize(200,200)
+win.title("GUI")
+win.mainloop()
---
+++ 2.py
@@ -0,0 +1,33 @@
+'''def fyit_98(name):
+    print(name+"FYIT")
+fyit_98("Rohini") 
+
+
+def my_function(name,roll_no):
+    print("first name"+name)
+    print("ROLLno",roll_no)
+my_function("rohini",98)
+
+
+def info(fname,lname,roll_no):
+    print("first name"+fname)
+    print("last name"+lname)
+    print("ROLLno",roll_no)
+info("rohini","Singh",98)'''
+
+
+
+
+
+
+
+
+
+num=int(input("enter a number"))
+parameter=4*num
+print("the parameter of square is:",parameter)
+
+r=int(input("enter a number"))
+pi=3.14
+parameter=(2*pi*r)
+print("the parameter of circle is:",parameter)
